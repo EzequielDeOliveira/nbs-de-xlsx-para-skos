da5fc0a776dfd9173cf749a7d0b5326861087b17
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2018-11-13 17:43:11 -0200

    funçao para retornar nó pai a partir do codigo nbs Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Nitai Bezerra <nitaibezerra@protonmail.com>

---
+++ findParentNodeByNbsCode.py
@@ -0,0 +1,5 @@
+def parentNodeByNbs(codes):
+    codes = codes.replace('.','')
+    codes = codes.rstrip('0')
+    codes = codes[:-1]
+    return codes
--- script.py
+++ script.py
@@ -12,15 +12,19 @@ nebs = xl.parse('NEBS')
 graph = Graph()
 skos = Namespace('http://www.w3.org/2004/02/skos/core#')
 graph.bind('skos', skos)
-
-for a in range(0, 1236):
-    graph.add((URIRef('URI'), RDF['type'], skos['Concept']))
-    graph.add((URIRef('URI'), skos['prefLabel'], Literal(nebs.DESCRIÇÃO.get(a), lang='pt-br')))
-    graph.add((URIRef('URI'), skos['prefLabel'], Literal(nebs.NBS2.get(a), lang='pt-br')))
-    graph.add((URIRef('URI'), skos['related'], URIRef('URI-Related')))
+i = 0
+for a in range(len(nebs)):
+    i += 1
+    uri = 'http://vocab.mdic.gov.br/NBS/v2.0/#' + ......
+    graph.add((URIRef(uri), RDF['type'], skos['Concept']))
+    graph.add((URIRef(uri), skos['prefLabel'], Literal(nebs.DESCRIÇÃO.get(a), lang='pt-br')))
+   """  graph.add((URIRef(uri), skos['broader'], Literal(nebs.NBS2.get(a), lang='pt-br'))) """
+    graph.add((URIRef(uri), skos['related'], URIRef('URI-Related')))
 
 print(graph.serialize(format='pretty-xml'))
 
 file = open('testfile.xml','w') 
 
 file.write(str(graph.serialize(format='pretty-xml')))
+
+print(i)
