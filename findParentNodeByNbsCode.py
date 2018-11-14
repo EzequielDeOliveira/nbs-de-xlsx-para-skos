e59a6eb29410ef9ac5572dc1c76ef9e6b0fdd5b2
Nitai Bezerra <nitaibezerra@protonmail.com>
2018-11-14 09:53:12 -0200

    Refatoração do código e correção do algoritmo
     Signed-off-by: Nitai Bezerra <nitaibezerra@protonmail.com>
    
     Co-authored-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

--- findParentNodeByNbsCode.py
+++ findParentNodeByNbsCode.py
@@ -1,5 +1,21 @@
-def parentNodeByNbs(codes):
-    codes = codes.replace('.','')
-    codes = codes.rstrip('0')
-    codes = codes[:-1]
-    return codes
+def parentCodeByNbs(code):
+        strTmp = code.replace('.','')
+        strTmp = strTmp.rstrip('0')
+        
+        if len(strTmp) <= 3:
+            return 1
+        elif len(strTmp) <= 6:
+            if len(strTmp) <= 5:
+                strTmp = strTmp[:-2]
+            else:
+                strTmp = strTmp[:-1]
+            strTmp = strTmp[0] + '.' + strTmp[1:5]
+        else:
+            strTmp = strTmp[:-1]
+            strTmp = strTmp[0] + '.' + strTmp[1:11]
+            if len(strTmp) >= 6:
+                strTmp = strTmp[0:6] + '.' + strTmp[6:11]
+            
+                if len(strTmp) > 9:
+                    strTmp = strTmp[0:9] + '.' + strTmp[9:11]
+        return strTmp
